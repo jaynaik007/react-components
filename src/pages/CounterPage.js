a0753db7cfc3e77fee98eb414266b0f065e63f5c
Jayendra Naik <jayendranaik007@gmail.com>
2023-11-26 23:28:28 +0530

    useReducer-done

--- src/pages/CounterPage.js
+++ src/pages/CounterPage.js
@@ -1,13 +1,102 @@
-import useCounter from "../hooks/use-counter";
 import Button from "../components/Button";
+import { useReducer } from "react";
+import Panel from "../components/Panel";
+// import { produce } from "immer";
+
+const INCREMENT_COUNT = "increment";
+const DECREMENT_COUNT = "decrement";
+const SET_VALUE_TO_ADD = "set-value-to-add";
+const ADD_FROM_LOT = "add-from-lot";
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case INCREMENT_COUNT:
+      return {
+        ...state,
+        count: state.count + 1,
+      };
+
+    case DECREMENT_COUNT:
+      return {
+        ...state,
+        count: state.count - 1,
+      };
+
+    case SET_VALUE_TO_ADD:
+      return {
+        ...state,
+        valueToAdd: action.payload,
+      };
+
+    case ADD_FROM_LOT:
+      return {
+        ...state,
+        count: state.count + state.valueToAdd,
+        valueToAdd: 0,
+      };
+
+    default:
+      return state; // you can also raise an exception here
+  }
+};
 
 function CounterPage({ initialCount }) {
-  const { count, increment } = useCounter({ initialCount });
+  //   const [count, setCount] = useState(initialCount);
+  //   const [valueToAdd, setValueToAdd] = useState(0);
+  const [state, dispatch] = useReducer(reducer, {
+    count: initialCount,
+    valueToAdd: 0,
+  }); // useReducer is like useState but it contains multiple variables in state object
+
+  const increment = () => {
+    dispatch({
+      type: INCREMENT_COUNT,
+    });
+  };
+
+  const decrement = () => {
+    dispatch({
+      type: DECREMENT_COUNT,
+    });
+  };
+
+  const handleChange = (event) => {
+    const value = parseInt(event.target.value);
+    dispatch({
+      type: SET_VALUE_TO_ADD,
+      payload: value,
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    // setCount(count + valueToAdd);
+    // setValueToAdd(0);
+    dispatch({
+      type: ADD_FROM_LOT,
+    });
+  };
+
   return (
-    <div>
-      <h1> Count is {count}</h1>
-      <Button onClick={increment}>Increment</Button>
-    </div>
+    <Panel className="m-3">
+      <h1 className="text-lg"> Count is {state.count}</h1>
+      <div className="flex flex-row">
+        <Button onClick={increment}>Increment</Button>
+        <Button onClick={decrement}>Decrement</Button>
+      </div>
+
+      <form onSubmit={handleSubmit}>
+        <label>Add a lot!</label>
+        <input
+          type="number"
+          value={state.valueToAdd || ""}
+          onChange={handleChange}
+          className="p-1 m-3 bg-gray-50 border border-gray-300"
+        />
+        <Button>Add it</Button>
+      </form>
+    </Panel>
   );
 }
 
